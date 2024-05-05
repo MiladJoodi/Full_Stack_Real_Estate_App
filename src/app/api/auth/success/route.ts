9400bd17fe2c77d8ce2d9b1211e5132de6ad5d9a
Milad Joodi <winbeta.com@gmail.com>
2024-05-05 21:26:35 +0330

    Update route.ts

--- src/app/api/auth/success/route.ts
+++ src/app/api/auth/success/route.ts
@@ -3,30 +3,28 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const { getUser } = await getKindeServerSession();
-    const user = await getUser();
+  const { getUser } = await getKindeServerSession();
+  const user = await getUser();
 
-    if (!user || user === null || !user.id) {
-        throw new Error("Something went with authentication");
-    }
+  if (!user || user === null || !user.id)
+    throw new Error("Something went wrong with authentication" + user);
 
-    const dbUser = await prisma.user.findUnique({
-        where: {
-            id: user.id
-        },
+  const dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+      },
     });
 
-    if (!dbUser) {
-        await prisma.user.create({
-            data: {
-                id: user.id,
-                firstName: user.given_name ?? "",
-                lastName: user.family_name ?? "",
-                email: user.email ?? "",
-            }
-        });
-
-        return NextResponse.redirect("https://real-estate-sand-eta.vercel.app/")
-    }
-
+    return NextResponse.redirect("http://localhost:3000/");
+  }
 }
