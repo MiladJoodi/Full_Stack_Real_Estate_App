c967817d3e7da35301e53c3d348f7d147ca40b37
Milad Joodi <winbeta.com@gmail.com>
2024-05-07 00:45:19 +0330

    up

---
+++ src/app/components/UserProfilePanel.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { User as PrismaUser } from "@prisma/client";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Avatar,
+  User,
+} from "@nextui-org/react";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+
+interface Props {
+  user: PrismaUser;
+}
+
+const UserProfilePanel = ({ user }: Props) => {
+  return (
+    <Dropdown placement="bottom-start">
+      <DropdownTrigger>
+        <User
+          as="button"
+          avatarProps={{
+            isBordered: true,
+            src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+          }}
+          className="transition-transform"
+          description="@tonyreichert"
+          name="Tony Reichert"
+        />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="User Actions" variant="flat">
+        <DropdownItem key="logout" color="danger">
+          <LogoutLink>Log Out</LogoutLink>
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export default UserProfilePanel;
--- src/app/components/signInPanel.tsx
+++ src/app/components/signInPanel.tsx
@@ -1,23 +1,42 @@
-import { LoginLink, LogoutLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { Button } from "@nextui-org/react";
+import UserProfilePanel from "./UserProfilePanel";
+import prisma from "@/lib/prisma";
 
 const SignInPanel = async () => {
-    const {isAuthenticated, getUser} = await getKindeServerSession();
-    const user = await getUser();
-    if(await isAuthenticated()) return <div>{user?.given_name} 
-     <LogoutLink> Logout</LogoutLink>
-    </div>;
-
+  const { isAuthenticated, getUser } = await getKindeServerSession();
+  const user = await getUser();
+  const dbUser = await prisma.user.findUnique({
+    where: {
+        id: user?.id,
+    },
+  });
+  if (await isAuthenticated()){
+    
+  }
     return (
-        <div className="flex gap-3">
-            <Button color="primary">
-                <LoginLink>Sign In</LoginLink>
-            </Button>
-            <Button>
-                <RegisterLink>Sign Up</RegisterLink>
-            </Button>
-        </div>
+      <>
+        {dbUser!! &&
+        <UserProfilePanel user={dbUser} />
+      }
+      </>
     );
-}
+
+  return (
+    <div className="flex gap-3">
+      <Button color="primary">
+        <LoginLink>Sign In</LoginLink>
+      </Button>
+      <Button>
+        <RegisterLink>Sign Up</RegisterLink>
+      </Button>
+    </div>
+  );
+};
 
 export default SignInPanel;
