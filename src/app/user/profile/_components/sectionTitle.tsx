71945e743beea7084df622d5ce7f6ab47e0e1e0c
Milad Joodi <winbeta.com@gmail.com>
2024-05-09 14:19:28 +0330

    up

--- src/app/user/profile/_components/sectionTitle.tsx
+++ src/app/user/profile/_components/sectionTitle.tsx
@@ -5,7 +5,7 @@ interface Props {
 const SectionTitle = (props: Props) => {
     return (
         <div>
-            <h1>{props.title}</h1>
+            <h1 className="text-xl font-bold text-slate-500">{props.title}</h1>
             <hr className="border-slate-400 my-2 border-solid" />
         </div>
     );
